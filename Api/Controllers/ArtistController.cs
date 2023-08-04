db4efd920b92488c8f8277ad9982a834e381f30e
Kirill Taran <kirilltaran@hotmail.com>
2023-08-04 18:48:29 +0400

    Artist controller added

---
+++ Api/Controllers/ArtistController.cs
@@ -0,0 +1,40 @@
+﻿using Core.Models.Artists;
+using Core.Services.Artists;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Controllers;
+
+[Route("api/v{version:apiVersion}/artists")]
+[Produces("application/json")]
+[ApiController]
+public sealed class ArtistController : BaseController
+{
+    public ArtistController(IArtistService artistService)
+    {
+        _artistService = artistService;
+    }
+
+
+    [HttpPost("spotifyId")]
+    [ProducesResponseType(typeof(Artist), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Add([FromRoute][Required] string spotifyId, CancellationToken cancellationToken = default)
+    {
+        var context = GetManagerContext();
+
+        return OkOrBadRequest(await _artistService.Add(context, spotifyId, cancellationToken));
+    }
+
+
+    [HttpGet("artistId")]
+    [ProducesResponseType(typeof(Artist), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Get([FromRoute][Required] int artistId, CancellationToken cancellationToken = default)
+    {
+        var context = GetManagerContext();
+
+        return OkOrNotFoundOrBadRequest(await _artistService.Get(context, artistId, cancellationToken));
+    }
+
+
+    private readonly IArtistService _artistService;
+}
---
+++ Api/Controllers/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using Core.Models.Labels;
+using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class BaseController : ControllerBase
+{
+    protected ManagerContext GetManagerContext()
+    {
+        return new ManagerContext
+        {
+            Id = 1,
+            LabelId = 1
+        };
+    }
+
+
+    protected IActionResult OkOrBadRequest<T>(Result<T> result)
+    {
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+            
+        return Ok(result.Value);
+    }
+
+
+    protected IActionResult OkOrNotFoundOrBadRequest<T>(Result<T> result)
+    {
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+
+        var value = result.Value;
+        if (value is null || value.Equals(default))
+            return NotFound();
+            
+        return Ok(value);
+    }
+}
